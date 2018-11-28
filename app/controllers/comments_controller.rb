ae1edd681be85da2810782b8688045a9741b7961
Thu Le <lethikhanhthu@gmail.com>
2018-11-28 19:46:15 +0100

    Add create action and comment form in view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,15 @@
 class CommentsController < ApplicationController
   before_action :set_movie
+  before_action :comment_params, only: :create
 
   def create
+    @comment = @movie.comments.new(comment_params.merge(author: current_user))
+    if @comment.save
+      flash[:notice] = 'Comment was successfully saved!'
+    else
+      flash[:error] = 'Comment cannot be empty!'
+    end
+    redirect_to movie_path(@movie)
   end
 
   def destroy
@@ -11,5 +19,9 @@ class CommentsController < ApplicationController
 
   def set_movie
     @movie = Movie.find(params[:movie_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(%i[body author movie])
   end
 end
