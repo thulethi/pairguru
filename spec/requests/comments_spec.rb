6bc32faf7106996891fa323377a05122fefa7776
Thu Le <lethikhanhthu@gmail.com>
2018-11-28 17:51:54 +0100

    Add comment request spec skeleton

---
+++ spec/requests/comments_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Comments requests', type: :request do
+  describe 'comments list' do
+    it 'displays right title'
+    it 'displays comments list of selected movie'
+    it 'POST /comments'
+    it 'DELETE /comments'
+  end
+end
