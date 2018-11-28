090b84bcd39640a45d420e9267434babc5c5f3cd
Thu Le <lethikhanhthu@gmail.com>
2018-11-28 18:38:48 +0100

    Add comments list example to comment request spec

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Comments requests', type: :request do
+  let(:selected_movie) {FactoryBot.create :movie, id: rand(1..10) }
+
   describe 'comments list' do
-    it 'displays right title'
-    it 'displays comments list of selected movie'
+    comments_count = rand(5..15)
+    let!(:comments) { FactoryBot.create_list(:comment, comments_count, movie: selected_movie) }
+
+    it 'displays comments list of selected movie' do
+      visit '/movies/' + selected_movie.id.to_s
+      expect(page).to have_selector('h3', text: 'Comments')
+      expect(page).to have_selector('.comment', count: comments_count)
+    end
+
     it 'POST /comments'
     it 'DELETE /comments'
   end
