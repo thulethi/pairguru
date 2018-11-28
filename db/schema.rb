20fbaf668050a7613d1d5418e0555693a8119f9b
Thu Le <lethikhanhthu@gmail.com>
2018-11-28 16:35:03 +0100

    Create migration for comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406223727) do
+ActiveRecord::Schema.define(version: 2018_11_28_153124) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "author_id"
+    t.integer "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["movie_id"], name: "index_comments_on_movie_id"
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
